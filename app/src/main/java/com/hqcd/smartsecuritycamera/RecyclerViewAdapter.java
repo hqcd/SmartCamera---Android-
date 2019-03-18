d2a805c19e372f05471e4cb1f236cbaeab267713
Quinten <duongquinten@gmail.com>
2019-03-17 20:21:45 -0400

    Main screen modified to include recent activity. Login and Register functions split into 2 activities

---
+++ app/src/main/java/com/hqcd/smartsecuritycamera/RecyclerViewAdapter.java
@@ -0,0 +1,75 @@
+package com.hqcd.smartsecuritycamera;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(ArrayList<String> mImageNames, ArrayList<String> mImages, Context mContext) {
+        this.mImageNames = mImageNames;
+        this.mImages = mImages;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listitem, viewGroup, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mImages.get(i));
+        viewHolder.image.setImageBitmap(bitmap);
+        viewHolder.name.setText(mImageNames.get(i));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+
+        CircleImageView image;
+        TextView name;
+        RelativeLayout parentLayout;
+        public ViewHolder(View itemView){
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            name = itemView.findViewById(R.id.text);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+
+    }
+
+
+}
